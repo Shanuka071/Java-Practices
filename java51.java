78af11695f52c9422da06a35c6186e0303064389
Shanuka071 <Shanukaaspu@gmail.com>
2025-08-22 14:45:34 +0530

    The Numbers from 1 to 10

---
+++ java51.java
@@ -0,0 +1,15 @@
+public class java51 {
+ 
+    public static void main(String[] args){
+        int number=0;
+
+         for(int i=0;i<10;i++){
+            
+            number=number+i;
+        
+
+        }
+        System.out.println("The sum of numbers from 1 to 10 is "+number);
+    }
+    
+}
