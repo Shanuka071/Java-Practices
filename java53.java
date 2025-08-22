74146f282c071b79de0903035a37c6d98eeadb10
Shanuka071 <Shanukaaspu@gmail.com>
2025-08-22 15:09:28 +0530

    Addition of square numbers from 1 to 20

---
+++ java53.java
@@ -0,0 +1,21 @@
+public class java53 {
+    public static void main(String[] args){
+        // addition of Square numbers from 1 to 20
+
+        int Sum=0;
+        for(int i=0;i<20;i++){
+            int Square;
+            
+
+            Square=i*i;
+            Sum=Sum+Square;
+
+            System.out.println("The Square numbers from 1 to 20 "+Square);
+            
+
+
+        }
+         System.out.println("The sum of square numbers from 1 to 20 "+Sum);
+    }
+    
+}
