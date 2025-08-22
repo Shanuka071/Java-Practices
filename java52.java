52c5c532c1b6f364c63fe944c1d913015ff8b93b
Shanuka071 <Shanukaaspu@gmail.com>
2025-08-22 14:22:32 +0530

    Square Numbers from zero to hundred

---
+++ java52.java
@@ -0,0 +1,14 @@
+public class java52 {
+    //the square numbers from zero to hundred.
+    public static void main(String[] args){
+         
+        for(int i=0;i<100;i++){
+            int Square;
+            Square=i*i;
+            System.out.println("The square numbers from 1 to 100 "+i+"th number is "+Square);
+
+        }
+        
+
+    }
+}
