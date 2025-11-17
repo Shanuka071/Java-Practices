a4386ce7e5ca4faebd6e90ecee5b80baabbfeccc
Shanuka071 <shanukaaspu@gmail.com>
2025-11-17 13:06:56 +0530

    feat:Increase the simple calulator with user expectation

--- JavaExercise3.java
+++ JavaExercise3.java
@@ -21,7 +21,8 @@ static double[] number1(int number){
     }
     return number1;
 }
-    static int operator(){
+    
+static int operator(){
         
         System.out.println("1.Addition");
         System.out.println("2.Substraction");
@@ -29,11 +30,11 @@ static double[] number1(int number){
         System.out.println("4.Division");
         System.out.println("Select the Operation number :");
 
-        int operator=obj1.nextInt();
-        return operator;
+        int operator1=obj1.nextInt();
+        return operator1;
     }
     
-    static double result(int operator,double[] number1,int number){
+    static double result(int operator,int operator1,double[] number1,int number){
         double number3=number1[0]; 
         switch (operator) {
             case 1:
@@ -80,11 +81,80 @@ static double[] number1(int number){
         return number3;
 
     }
+        static double result1(int operator,int operator1,double[] number1,int number){
+        double number4=number1[0]; 
+        switch (operator) {
+            case 1:
+                number4=0;
+                for(int i=0;i<number1.length;i++){
+                    
+                    number4+=number1[i];
+                }
+                System.out.println("The  the number is "+number4);
+            
+                break;
+             case 2:
+                number4=number1[0];
+                for(int i=1;i<number1.length;i++){
+                    
+                    number4-=number1[i];
+                }
+                System.out.println("The differentiation number are "+number4);
+            
+                break;
+            case 3:
+                number4=number1[0];
+                for(int i=1;i<number1.length;i++){
+                    number4*=number1[i];
+                }
+                System.out.println("The Mulitiplication of the number is "+number4);
+                break;
+            case 4:
+                number4=number1[0];
+                for(int i=1;i<number1.length;i++){
+                    number4/=number1[i];
+                }
+                System.out.println("The division of  is "+number4);
+                
+                break;
+                
+                
+                
+            default:
+                System.out.println("Invalid operator");
+                return 0;
+                
+        }
+        return number4;
+
+    }
+
+
+    
     public static void main(String[] args){
         int number=number();
         double[] number1=number1(number);
-        int operator=operator();
-        double number3=result(operator, number1, number);
+        int operator1=operator();
+        double number3=result1(operator1, operator1, number1, number);
+        
+        int desire=1;
+        
+        //control the calculator by the user.
+        while(desire==1){
+            
+            System.out.println("1.if you preffer to change the operator ");
+            System.out.println("2.You can end the calculation.");
+            desire=obj1.nextInt();
+            if(desire==1){
+                int operator=operator();
+            double number4=result(operator,operator1, number1, number);
+            }
+            else{
+                System.out.println("The Calculation is over");
+            }
+        }
+
+        
 
  
     }
