8c38ea8f48d0e8f940b947895c470da8486d620d
Shanuka071 <shanukaaspu@gmail.com>
2025-11-19 21:41:11 +0530

    feat:A program for saying love.

---
+++ JavaExercise7.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class JavaExercise7 {
+    static Scanner obj1=new Scanner(System.in);
+    public static void main(String[] args){
+        System.out.println("Enter the number:");
+        
+        int number=obj1.nextInt();
+
+        for(int i=0;i<=number;i++){
+            System.out.println("Ane inna");
+            System.out.println("Tharaha wenna epa Ane mama paune.");
+        }
+    }
+    
+}
