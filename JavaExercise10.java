f8d4e5c01840f4bc6cabca7bab61d49f00a74f46
Shanuka071 <shanukaaspu@gmail.com>
2025-11-24 11:24:57 +0530

    feat:i don't get the right love forerver.

---
+++ JavaExercise10.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class JavaExercise10 {
+    static Scanner obj1=new Scanner(System.in);
+    public static void main(String[] args){
+        System.out.println("Enter the number");
+        int number1=obj1.nextInt();
+        
+        for(int i=0;i<number1;i++){
+            System.out.println("The love is not valid for umesh. That is the truth.");
+            System.out.println("Please, Remember that. The love isn't valid for you umesh.You have to achieve some thing to get some thing.that's it.");
+        }
+    }
+    
+
+}
