c0f17f0d6b9eb185a47abe29dab3ceca94c13cfc
Shanuka071 <shanukaaspu@gmail.com>
2025-11-29 22:25:38 +0530

    feat:modify the program with some functions

--- JavaExercise12.java
+++ JavaExercise12.java
@@ -3,7 +3,7 @@ public class JavaExercise12 {
     
     static Scanner obj1=new Scanner(System.in);
 
-static int select_vehicle(){
+static int select_package(){
     System.out.println("********Welcome to A.S.P. Wedding planners.****************");
     System.out.println("Select the Package of our service,");
     System.out.println("1.Primum package ");
@@ -35,8 +35,8 @@ static void package_details( int number){
         case 3:
             System.out.println("*******Thank you for selecting the Normal Package*******");
             System.out.println("Here are our services that are belong to the Normal Package..."); 
-            System.out.println("1.Hotel Services...");
-            System.out.println("2.A buffer set for the 100 plates...");
+            System.out.println("2.Hotel Services...");
+            System.out.println("3.A buffer set for the 100 plates...");
             
             break;
         default:
@@ -45,6 +45,79 @@ static void package_details( int number){
     }
 
 }
+    static void package_deatails1(int number){
+        System.out.println("....Futher more details....");
+        System.out.println("Enter number ");
+        int number2=obj1.nextInt();
+
+        switch (number2) {
+            case 1:
+                System.out.println("...Select the vehicle...");
+                System.out.println("1.Audi Car.");
+                System.out.println("2.Mercedize Benz.");
+                System.out.println("3.BMW.");
+                System.out.println("4.Prius.");
+                int number3=obj1.nextInt();
+               //choose the customer's choice Car...
+
+               switch (number3) {
+                case 1:
+                    System.out.println("*There is a free 100Km ");
+                    System.out.println("*There is a extra charge for additional km");
+                    double price1=10000.00;
+                    System.out.println("*The price of car is Rs."+price1);
+                    break;
+                case 2:
+                    System.out.println("*There is a free 100Km ");
+                    System.out.println("*There is a extra charge for additional km");
+                    double price2=10000.00;
+                    System.out.println("*The price of car is Rs."+price2);
+
+                    break;    
+                case 3:
+                    System.out.println("*There is a free 100Km ");
+                    System.out.println("*There is a extra charge for additional km");
+                    double price3=10000.00;
+                    System.out.println("*The price of car is Rs."+price3);
+                    break;
+
+                case 4:
+                    System.out.println("*There is a free 100Km ");
+                    System.out.println("*There is a extra charge for additional km");
+                    double price4=10000.00;
+                    System.out.println("*The price of car is "+price4);
+                    break;    
+                default:
+                    System.out.println("Please a enter the valid number for this.");
+                    break;
+               }
+
+                break;
+            case 2:
+                System.out.println("........Hotel Services.....");
+                System.out.println("*All the Hotel Servies are free of Charge...");
+                System.out.println("*There is no any extra payment for this package...");
+                System.out.println("*You can customerize the services also.");
+                break;
+            case 3:
+                System.out.println(".......BufferSet.........");
+                System.out.println("*Custermer can select the buffer menu as thier wishes...");
+                System.out.println("*And, dessert coner is available in this package...");
+                System.out.println("*Dinning tables deccorations...");       
+                System.out.println("*Buffer set includes only 100 plates only...");
+                System.out.println("*For additional chargers are charged per each plates...");
+            case 4:
+                System.out.println(".........DJ Sound system.........");
+                System.out.println("*The Dj sound system with free artist is given...");
+                System.out.println("*Free smoking machine...");
+                System.out.println("*free light system is provided...");
+                System.out.println("*Free wireless mics are supplied...");
+                break;    
+            default:
+                System.out.println("Enter the valid number for continue the programme...1");
+                break;
+        }
+    }
 
     static double wedding_calculator(int number){
         System.out.println("Please enter your budget:");
@@ -55,10 +128,9 @@ static void package_details( int number){
 
         switch (number1) {
             case 1:
-                int free_distance=100;
-                double distance=0;
-                double fuel_cost=5000;
-                double wedding_car_cost=distance*75;
+            
+
+
                 break;
         
             default:
@@ -67,5 +139,11 @@ static void package_details( int number){
         
         return 0;
         //
+    }
+
+    public static void main(String[] args){
+        int number=select_package();
+        package_details(number);
+        package_deatails1(number);
     }
 }
